3e3f3a271e64c15b162447d45cb58a672516e7f5
josuel A. Lopes <josuellions@gmail.com>
2023-06-21 03:04:44 -0300

    Criando primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <h1>Bem Vindo</h1>
+            <p>Se hoje está dificil, lembre-se que hoje você não está onde estava ontem e certamente amanhã não vai estar onde está hoje.</p>
+            <p>Siga em frete tudo passa.</p>
+        </>
+    )
+}
+
+export default Home;
