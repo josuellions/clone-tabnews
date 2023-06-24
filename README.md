3c3bab0b3509d220a6396168d9043daf5a2a1a94
josuel A. Lopes <josuellions@gmail.com>
2023-06-24 19:50:16 -0300

    #2 - add scripts prettier `link:check` e `link:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnew
+
 Projeto clone tabnews filipe deshamps para estudo academico do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ function Home() {
   return (
     <>
       <h1>Bem Vindo</h1>
-      <p>Se hoje está dificil, lembre-se que hoje você não está onde estava ontem e certamente amanhã não vai estar onde está hoje.</p>
+      <p>
+        Se hoje está dificil, lembre-se que hoje você não está onde estava ontem
+        e certamente amanhã não vai estar onde está hoje.
+      </p>
       <p>Siga em frete tudo passa.</p>
     </>
-  )
+  );
 }
 
 export default Home;
