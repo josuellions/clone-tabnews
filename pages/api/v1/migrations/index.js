d6709a172f0eaeb1bf8fc58155d3d3465dbcdee1
josuel A. Lopes <josuellions@gmail.com>
2024-10-29 17:35:44 -0300

    #11 fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,7 +6,6 @@ export default async function migrations(req, res) {
   const allowedMethods = ["GET", "POST"];
 
   if (!allowedMethods.includes(req.method)) {
-    console.log(">>NOT METHOD");
     return res.status(405).json({
       error: `Method "${req.method}" not allowed`,
     });
