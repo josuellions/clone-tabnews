34d60bef5651db36ed3a940d60de75c238bc069c
josuellions <josuellions@gmail.com>
2025-08-22 15:17:10 -0300

    feat: #28 - create `user` model and `/api/v1/users` endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,4 +1,5 @@
-import database from "infra/database";
+import { version as uuidVersion } from "uuid";
+
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
@@ -10,28 +11,113 @@ beforeAll(async () => {
 describe("POST to /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: [
-          "josuellopes",
-          "josuellopes@email, password.com.br",
-          "senha123",
-        ],
-      });
-
-      // await database.query({
-      //   text: "INSERT INTO users (username, email) VALUES ($1, $2, $3);",
-      //   values: ["josuelalves", "Josuellopes@email.com.br", "senha123"],
-      // });
-
-      const users = await database.query("SELECT * FROM users;");
-
-      console.info(">>TEST USERS");
-      console.log(users.rows);
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "josuellopes",
+          email: "josuellopes@email.com.br",
+          password: "senha123",
+        }),
       });
+
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "josuellopes",
+        email: "josuellopes@email.com.br",
+        password: "senha123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      const versionUUID = 4;
+      expect(uuidVersion(responseBody.id)).toBe(versionUUID);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With duplicated 'email'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado",
+          email: "emailduplicado@email.com.br",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado2",
+          email: "EmailDuplicado@email.com.br",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está cadastrado.",
+        action: "Informe outro email para realizar o cadastro.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicated 'username'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "usernameduplicado",
+          email: "usernameduplicado1@email.com.br",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "UsernameDuplicado",
+          email: "usernameduplicado2@email.com.br",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O username informado já está cadastrado.",
+        action: "Informe outro username para realizar o cadastro.",
+        status_code: 400,
+      });
     });
   });
 });
